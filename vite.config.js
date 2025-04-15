c8d4a5ac1698b697e5fbd5e0dd4625fd95a2a679
Javan Wang <javan.z.wang@gmail.com>
2025-04-15 16:20:57 -0400

    Deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/time-zone-converter/',
   server: {
     host: true,
     allowedHosts: [
